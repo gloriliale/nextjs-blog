b263d567e6e93f1f563621be6173702a4b5d0bb2
Glorili Alejandro <glorili.alejandro@gmail.com>
2023-09-06 14:42:53 -0700

    change title

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Glori Lili";
+const name = "Glorimade";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home = false }) {
